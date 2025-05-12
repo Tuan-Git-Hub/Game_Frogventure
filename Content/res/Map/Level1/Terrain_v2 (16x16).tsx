29a6dd7f17b713fc4fb91b5e2fecc6c3ac8f63cc
Tuan-Git-Hub <maianhtuan96.nd@gmail.com>
2025-05-12 16:28:39 +0700

    Completion date: 28/4/2025

---
+++ Content/res/Map/Level1/Terrain_v2 (16x16).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Terrain_v2 (16x16)" tilewidth="16" tileheight="16" tilecount="374" columns="34">
+ <image source="Terrain_v2 (16x16).png" width="544" height="176"/>
+</tileset>
